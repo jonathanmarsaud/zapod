b4dd68b559bd4ea391a2ea26d0c323e5e5d2dadc
Jonathan Marsaud <zic@marsaud.org>
2018-01-07 11:29:53 +0100

    Manage when bmp: Bitmap object is null properly

--- app/src/main/java/org/marsaud/zapod/MainActivity.kt
+++ app/src/main/java/org/marsaud/zapod/MainActivity.kt
@@ -29,7 +29,12 @@ class MainActivity : AppCompatActivity() {
         StrictMode.setThreadPolicy(policy)
 
         bmp = getImage(getPage(baseUrl + "astropix.html"))
-        apod.setImageBitmap(bmp)
+        if (bmp != null) {
+            apod.setImageBitmap(bmp)
+        } else {
+            setWallpaperButton.visibility = View.GONE
+            errorTextView.visibility = View.VISIBLE
+        }
     }
 
 
@@ -49,7 +54,7 @@ class MainActivity : AppCompatActivity() {
 
             // Parse webpage via JSoup
             val document = Jsoup.parse(responseString)
-            val element = document.select("a")[1] // using get(1) instead of first() (= 0) to have the second element
+            val element = document.select("a")[0] // using get(1) instead of first() (= 0) to have the second element
             val parsedString = element.attr("href")
 
             return baseUrl + parsedString
