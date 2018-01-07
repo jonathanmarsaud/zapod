814299b3b2d2c582932bfb990b2375051926d32b
Jonathan Marsaud <zic@marsaud.org>
2018-01-07 16:51:54 +0100

    Add Intent to www.marsaud.org

--- app/src/main/java/org/marsaud/zapod/AboutActivity.kt
+++ app/src/main/java/org/marsaud/zapod/AboutActivity.kt
@@ -1,11 +1,24 @@
 package org.marsaud.zapod
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+    }
+
+    /**
+     * Visit www.marsaud.org, triggered by XML onClick attributes.
+     *
+     * @param view Mandatory signature function for onClick attributes on XML.
+     */
+    fun goToMarsaudOrg(view: View) {
+        val marsaudOrgIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.marsaud.org/"))
+        startActivity(marsaudOrgIntent)
     }
 }
