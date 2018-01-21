effceb269e31934e11d37e377980fda8ed351272
Jonathan Marsaud <zic@marsaud.org>
2018-01-21 16:04:04 +0100

    2.11: forget to change version in code...

--- app/src/main/java/org/marsaud/zapod/ZapodActivity.kt
+++ app/src/main/java/org/marsaud/zapod/ZapodActivity.kt
@@ -22,7 +22,7 @@ import org.jsoup.Jsoup
 class ZapodActivity : AppCompatActivity() {
     val baseUrl = "https://apod.nasa.gov/apod/"
     val client = OkHttpClient()
-    val version = "2.9"
+    val version = "2.11"
     var bmp: Bitmap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
