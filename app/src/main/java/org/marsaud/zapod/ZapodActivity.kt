f34aa2172dd9198fb3a1ff1c755a9d1086c8c99c
Jonathan Marsaud <zic@marsaud.org>
2018-04-21 12:58:20 +0200

    Test flag for PendingIntent of setRecurringAlarm()

--- app/src/main/java/org/marsaud/zapod/ZapodActivity.kt
+++ app/src/main/java/org/marsaud/zapod/ZapodActivity.kt
@@ -178,7 +178,7 @@ class ZapodActivity : AppCompatActivity() {
         val intent = Intent(context, AlarmReceiver::class.java)
         intent.putExtra("notification_content", getString(R.string.notification_content)) // R.string is not available through a class which is not an Activity
         intent.putExtra("notification_channeldescription", getString(R.string.notification_channeldescription)) // R.string is not available through a class which is not an Activity
-        val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT)
+        val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
         val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
         alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, updateTime.timeInMillis, AlarmManager.INTERVAL_DAY, pendingIntent)
     }
